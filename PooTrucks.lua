c0e3df9e4324fcef21702c889d23f9000d33528e
Jonny Paton <jonathan.r.paton@googlemail.com>
2021-09-26 20:33:14 +0100

    Formatting

--- PooTrucks.lua
+++ PooTrucks.lua
@@ -1,18 +1,14 @@
 ---Poo Trucks
-
 ---@author Jonny
 ---@version 0.0.0.1
 ---@date 25/09/2021
-
 ---@class PooTrucks
 PooTrucks = {}
 
 function PooTrucks:loadMap(name)
-    InGameMenuContractsFrame.onFrameOpen = Utils.overwrittenFunction(InGameMenuContractsFrame.onFrameOpen, self.onContractsFrameOpen)
-end
-
-function PooTrucks:onUpdate(dt)
-    print("PooTrucks update")
+    InGameMenuContractsFrame.onFrameOpen =
+        Utils.overwrittenFunction(InGameMenuContractsFrame.onFrameOpen,
+                                  self.onContractsFrameOpen)
 end
 
 function PooTrucks:onContractsFrameOpen(superFunc, ...)
@@ -20,24 +16,32 @@ function PooTrucks:onContractsFrameOpen(superFunc, ...)
     local missions = g_missionManager.missions
     print("Mission count: ", #missions)
 
-    local path = ('%ssavegame%d'):format(getUserProfileAppPath(), g_careerScreen.selectedIndex) .. "/poo.xml";
+    local path = ('%ssavegame%d'):format(getUserProfileAppPath(),
+                                         g_careerScreen.selectedIndex) ..
+                     "/poo.xml";
     print(path)
     local pooTrucks = createXMLFile("pooTrucks", path, "missions")
 
     for index, mission in ipairs(missions) do
         if mission.type.name == "harvest" then
-            --DebugUtil.printTableRecursively(mission)
+            -- DebugUtil.printTableRecursively(mission)
             mission.depositedLiters = mission.expectedLiters
-            setXMLString(pooTrucks, "missions.mission" .. index .. "#type", g_fillTypeManager:getFillTypeNameByIndex(mission.fillType))
-            setXMLFloat(pooTrucks, "missions.mission" .. index .. "#expectedLiters", mission.expectedLiters)
-            setXMLFloat(pooTrucks, "missions.mission" .. index .. "#depositedLiters", mission.depositedLiters)
+            setXMLString(pooTrucks, "missions.mission" .. index .. "#type",
+                         g_fillTypeManager:getFillTypeNameByIndex(
+                             mission.fillType))
+            setXMLFloat(pooTrucks,
+                        "missions.mission" .. index .. "#expectedLiters",
+                        mission.expectedLiters)
+            setXMLFloat(pooTrucks,
+                        "missions.mission" .. index .. "#depositedLiters",
+                        mission.depositedLiters)
         end
     end
 
     if pooTrucks and pooTrucks ~= 0 then
-		saveXMLFile(pooTrucks)
-		print("Saved poo trucks")
-	end
+        saveXMLFile(pooTrucks)
+        print("Saved poo trucks")
+    end
 end
 
 addModEventListener(PooTrucks);
